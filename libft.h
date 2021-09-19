9b6cb57bee55ab7766be2b6089eca9dde3c6321b
Daniel Poveda salgado <dpoveda-@c1r8s3.42madrid.com>
2021-09-19 17:58:30 +0200

    Update libft.h functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 13:32:11 by dpoveda-          #+#    #+#             */
-/*   Updated: 2021/09/17 00:30:45 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2021/09/19 17:58:03 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,7 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
 char	**ft_split(char const *s, char c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
@@ -61,13 +62,10 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_calloc(size_t count, size_t size);
 
 // Int functions
 int		ft_atoi(const char *str);
-char	*ft_itoa(int n);
-
-// Malloc functions
-void	*ft_calloc(size_t count, size_t size);
 
 // Fd functions
 void	ft_putchar_fd(char c, int fd);
