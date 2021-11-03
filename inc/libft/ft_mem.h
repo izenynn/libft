0bf6c74833b677c4b77f37de3d88d2d82c8e734f
izenynn <me@izenynn.com>
2021-11-03 21:11:47 +0100

    Forget to add ft_realloc to header

--- inc/libft/ft_mem.h
+++ inc/libft/ft_mem.h
@@ -22,5 +22,6 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_calloc(size_t count, size_t size);
+void	*ft_realloc(void *ptr, size_t size);
 
 #endif
