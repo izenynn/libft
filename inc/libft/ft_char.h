86f6dcc41e577ef6209ed1a042ab10b1894f7aa9
izenynn <me@izenynn.com>
2021-11-04 09:09:34 +0100

    Update ft_char.h

--- inc/libft/ft_char.h
+++ inc/libft/ft_char.h
@@ -15,8 +15,8 @@
 
 int	ft_islower(int c);
 int	ft_isupper(int c);
-int	ft_isblank(int c);
 int	ft_isspace(int c);
+int ft_isblank(int c);
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
