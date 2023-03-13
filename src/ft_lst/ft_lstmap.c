e421adc2f7b7a7a9edbcf4b7b24baa102ccc0395
Daniel Poveda salgado <dpoveda-@c1r13s5.42madrid.com>
2023-03-13 17:42:16 +0100

    fix: lstmap leak on malloc failure

--- src/ft_lst/ft_lstmap.c
+++ src/ft_lst/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 17:42:13 by dpoveda-          #+#    #+#             */
-/*   Updated: 2023/03/13 17:22:24 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2023/03/13 17:41:21 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
 	t_list	*newlst;
+	void	*data;
 
 	if (!lst || !f)
 		return (NULL);
 	newlst = NULL;
 	while (lst)
 	{
-		new = ft_lstnew(f(lst->data));
+		data = f(lst->data);
+		new = ft_lstnew(data);
 		if (!new)
 		{
+			del(data);
 			ft_lstclear(&newlst, del);
 			return (NULL);
 		}
